5d1a33db309700e388e8f05c318b8dcf7f5e4369
David Viloria Ortega <david.viloria@citi.com.mx>
2022-05-01 21:49:21 -0500

    1 - Se añade la posibilidad de poner gif en la App 2- Se añaden en un stackView la imagen y los label del header

--- Bankey/AccountSummaryHeaderView.swift
+++ Bankey/AccountSummaryHeaderView.swift
@@ -10,10 +10,12 @@ import UIKit
 class AccountSummaryHeaderView: UIView {
     
     @IBOutlet var contentView: UIView!
+    @IBOutlet weak var headerIcon: UIImageView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
+        loadGit()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -36,5 +38,10 @@ class AccountSummaryHeaderView: UIView {
         contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    func loadGit(){
+        let git = UIImage.gifImageWithName("Barad-Dur2")
+        headerIcon.image = git
     }
 }
